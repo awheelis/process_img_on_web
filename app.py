61649b70679b9309745868393f91e78d3cb1e45e
Alex Wheelis <alex_wheelis@Alexs-MacBook-Pro-4.local>
2022-08-05 12:35:09 -0600

    changes to host

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug = True, host='localhost', port=port)
